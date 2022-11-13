7274efa41b33efa0cdbe7087ee0c2e63ef5e911b
ThiagoA20 <thiagohca2019@gmail.com>
2022-11-13 18:26:45 -0300

    django project rest api

--- comandos.md
+++ comandos.md
@@ -1,3 +1,53 @@
 pip install django psycopg2 python-decouple djangorestframework
 django admin startproject test_rest .
 python manage.py runserver (8080 --> change the default port)
+python manage.py startapp myapp
+python manage.py makemigrations
+python manage.py migrate
+
+templates --> index.html
+app --> views.py
+
+def myview(request):
+    return render(request, "index.html")
+
+app --> urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('myview/', views.myview)
+]
+
+# Configurar PostgreSQL
+
+project --> settings.py --> DATABASES --> 'default':
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': '',
+'USER': '',
+'PASSWORD': '',
+'HOST': '',
+'PORT': ''
+
+# Configurar RestAPI
+
+em settings.py colocar 'rest_framework' dentro de INSTALLED_APPS
+
+criar pasta api, criar arquivo __init__.py
+criar arquivo views.py e colocar o seguinte conteúdo:
+######
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name': 'Thiago', 'age': 20}
+    return Response(person)
+######
+criar arquivo urls.py e colocar o seguinte conteúdo:
+######
+
+######
+dentro do arquivo urls.py do projeto importar a função include do django.urls e adicionar as urls da api dentro dos urlpatterns:
+path('', include('api.urls')),
--- test_rest/settings.py
+++ test_rest/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#by%7z6=5rk4q-r9)-ey9h-e&#h0k$6w1y5%k($s)^u=l8)k-_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'test_rest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "myapp/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'test_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
     }
 }
 
